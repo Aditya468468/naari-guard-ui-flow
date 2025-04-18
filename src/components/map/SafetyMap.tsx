e49a63a7e2c7e97a5907bf9b0e45fd153c2406f0
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2025-04-18 22:45:54 +0530

    Update SafetyMap.tsx

--- src/components/map/SafetyMap.tsx
+++ src/components/map/SafetyMap.tsx
@@ -3,11 +3,12 @@ import SafetyMapComponent from '../../map/SafetyMapComponent'; // Adjust the pat
 
 const SafetyMap: React.FC = () => {
   return (
-    <div>
-      <h1 className="text-center mt-4">Naari Guard Map</h1>
+    <div className="p-4">
+      <h1 className="text-center mt-4 text-3xl font-semibold text-gray-800">Naari Guard Map</h1>
       <SafetyMapComponent />
     </div>
   );
 };
 
 export default SafetyMap;
+
