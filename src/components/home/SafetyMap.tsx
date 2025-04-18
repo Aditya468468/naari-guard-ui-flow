cc21d2a3adccb5f1ced10206064babc2a1dda1bd
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2025-04-18 21:50:08 +0530

    Update SafetyMap.tsx

--- src/components/home/SafetyMap.tsx
+++ src/components/home/SafetyMap.tsx
@@ -1,6 +1,5 @@
-
 import React from 'react';
-import SafetyMapComponent from '../map/SafetyMap';
+import SafetyMapComponent from '../../map/SafetyMapComponent';  // Corrected the import path
 
 const SafetyMap: React.FC = () => {
   return <SafetyMapComponent />;
