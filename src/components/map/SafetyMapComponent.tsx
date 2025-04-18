4286d7abf7bebb3c68eb0cb023a980b25ecc5f85
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2025-04-18 21:47:38 +0530

    Create SafetyMapComponent.tsx

---
+++ src/components/map/SafetyMapComponent.tsx
@@ -0,0 +1,31 @@
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+
+// Fix for default Leaflet marker icon issue (important for Vite/React projects)
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconUrl: 'https://unpkg.com/leaflet@1.9.3/dist/images/marker-icon.png',
+  iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.3/dist/images/marker-icon-2x.png',
+  shadowUrl: 'https://unpkg.com/leaflet@1.9.3/dist/images/marker-shadow.png',
+});
+
+const SafetyMapComponent: React.FC = () => {
+  return (
+    <MapContainer
+      center={[12.9716, 77.5946]} // Use coordinates of the initial location (e.g., Bangalore)
+      zoom={13}
+      style={{ height: '100vh', width: '100%' }}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://osm.org/copyright">OpenStreetMap</a>'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[12.9716, 77.5946]}>
+        <Popup>You are here 🚨</Popup>
+      </Marker>
+    </MapContainer>
+  );
+};
+
+export default SafetyMapComponent;
