b57550c7a85e5f50e21f0b6cb268cd5ff7779287
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2025-04-19 01:48:02 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,11 @@ import App from './App.tsx'
 import './index.css'
 
 // 👇 Add this line
+// Fix Leaflet default icon issues in React/Vite apps
 import 'leaflet/dist/leaflet.css';
+import 'leaflet-defaulticon-compatibility';
+import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css';
+
 
 createRoot(document.getElementById("root")!).render(<App />);
 
